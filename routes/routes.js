8b7b4c7996650611a8eaf7431f75ac6e08443f67
anaaragon88 <ana.aragon88@gmail.com>
2024-10-09 10:15:41 +0200

    env example

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,12 @@ import {
   createBook,
   deleteBook,
 } from "../controllers/bookController.js";
+import { validateCreateBook } from "../validators/bookValidator.js";
 
 const bookrouter = express.Router();
 
 bookrouter.get("/book", getAllBooks);
-bookrouter.post("/create", createBook);
+bookrouter.post("/create", validateCreateBook, createBook);
 bookrouter.delete("/:id", deleteBook);
 
 export default bookrouter;
